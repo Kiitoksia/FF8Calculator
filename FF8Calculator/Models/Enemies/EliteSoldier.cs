ce845b33c780180536f669398f1fdc268ff06fc7
matt.ward92@gmail.com <matt.ward92@gmail.com>
2021-03-10 16:47:43 +0000

    Added Geezard/EliteSoldier

---
+++ FF8Calculator/Models/Enemies/EliteSoldier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF8Calculator.Models.Enemies
+{
+    public class EliteSoldier : EnemyModel
+    {
+        public EliteSoldier() : base(011, "Elite Soldier", new Element[] { }, new Element[] { })
+        {
+
+        }
+
+        public override string HPFormula => "0.4(Lv)² + 8(Lv)";
+
+        public override string StrengthFormula => "([5.2(Lv)] + [(Lv) / 5] - [(Lv)² / 260] + 7) / 4";
+
+        public override string MagicFormula => "([4.5(Lv)] + [(Lv) / 2] - [(Lv)² / 400] + 20) / 4";
+
+        public override string VitalityFormula => "((Lv) / 8) + 36";
+
+        public override string SpiritFormula => "((Lv) / 32) + 38";
+
+        public override string SpeedFormula => "((Lv) / 6) - [(Lv) / 12] + 6";
+
+        public override string EvasionFormula => "((Lv) / 6) - [(Lv) / 12] + 2";
+
+        public override string ExperienceFormula => "5(Lv)² + 25";
+    }
+}
---
+++ FF8Calculator/Models/Enemies/Geezard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF8Calculator.Models.Enemies
+{
+    public class Geezard : EnemyModel
+    {
+        public Geezard() : base(012, "Geezard", new Element[] { }, new Element[] { })
+        {
+
+        }
+
+        public override string HPFormula => "0.15(Lv)² + 3(Lv) + 40";
+
+        public override string StrengthFormula => "([2.8(Lv)] + [(Lv) / 5] - [(Lv)² / 180] + 18) / 4";
+
+        public override string MagicFormula => "([2.2(Lv)] + [(Lv) / 22] - [(Lv)² / 200] + 4) / 4";
+
+        public override string VitalityFormula => "((Lv) / 32) + 4";
+
+        public override string SpiritFormula => "((Lv) / 6) + 1";
+
+        public override string SpeedFormula => "((Lv) / 6) - [(Lv) / 12] + 3";
+
+        public override string EvasionFormula => "((Lv) / 7) - [(Lv) / 14]";
+
+        public override string ExperienceFormula => "5(Lv)² + 5";
+    }
+}
